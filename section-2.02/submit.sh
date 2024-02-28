96499e6f88aac74d827f8dbbb66c476b7ce0341b
jirudaya <j.irudayasamy@epcc.ed.ac.uk>
2024-02-28 12:57:48 +0000

    exer(training): modify submit script for ARCHER2
    Signed-off-by: jirudaya <j.irudayasamy@epcc.ed.ac.uk>

--- section-2.02/submit.sh
+++ section-2.02/submit.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+#SBATCH --job-name=HIP
+#SBATCH --gpus=1
 #SBATCH --time=00:01:00
+
+# Replace [budget code] below with your project code (e.g. t01)
+#SBATCH --account=[budget code] 
 #SBATCH --partition=gpu
 #SBATCH --qos=gpu-shd
-#SBATCH --gpus=1
 
-./a.out
-
+srun --ntasks=1 --cpus-per-task=1 ./a.out
