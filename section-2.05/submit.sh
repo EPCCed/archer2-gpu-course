03aecc553c41a8348ca4db62d22d95cdf08f451e
jirudaya <j.irudayasamy@epcc.ed.ac.uk>
2024-03-14 13:03:25 +0000

    exer(training): add job submission script
    Signed-off-by: jirudaya <j.irudayasamy@epcc.ed.ac.uk>

--- section-2.05/submit.sh
+++ section-2.05/submit.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+#SBATCH --job-name=HIP
+#SBATCH --gpus=1
 #SBATCH --time=00:01:00
+
+# Replace [budget code] below with your project code (e.g. t01)
+#SBATCH --account=z19
 #SBATCH --partition=gpu
-#SBATCH --qos=short
-#SBATCH --gres=gpu:1
+#SBATCH --qos=gpu-shd
 
-module load nvidia/nvhpc
-
-./a.out
-
+srun --ntasks=1 --cpus-per-task=1 ./a.out
